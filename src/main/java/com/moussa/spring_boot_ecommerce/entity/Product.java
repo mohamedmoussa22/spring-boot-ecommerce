530ce61c00141f4b8d33898f0a7b94d1c25407fa
Mohamed Moussa <mohamed.moussa2@dxc.com>
2025-03-07 20:08:50 +0200

    add ProductCategory Entity

--- src/main/java/com/moussa/spring_boot_ecommerce/entity/Product.java
+++ src/main/java/com/moussa/spring_boot_ecommerce/entity/Product.java
@@ -18,9 +18,9 @@ public class Product {
     @Column(name = "id")
     private Long id;
 
-//    @ManyToOne
-//    @JoinColumn(name = "category_id", nullable = false)
-//    private ProductCategory category;
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
 
     @Column(name = "sku")
     private String sku;
